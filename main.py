98412b2ce2913b0d919b7734efb71bbc1d56b0c8
Raj Shirolkar <rajshirolkar@gmail.com>
2022-10-30 14:18:50 -0700

    calendly integration

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import imp
+from email import message
+from re import S
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,24 @@ app = FastAPI()
 @app.get('/')
 async def hello():
     return {'Hello Hacksea'}
+
+
+# https://calendly.com/rajshirolkar/30min
+
+@app.get('/book-session')
+async def book_session():
+    session = {
+        'link': 'https://calendly.com/rajshirolkar/30min',
+        'counsellor name': 'Raj Shirolkar',
+        'Session Type': 'Video Counselling'
+    }
+    return session
+
+
+@app.get('/login')
+async def login(username: str, password: str):
+    return { 
+        message: 'Success',
+        username: username,
+        password: password
+        }
